749bf1a42d6079c690b62dfedff47f68d88e3bb0
Thomas Vincent <thomasvincent@gmail.com>
2026-02-22 01:53:46 -0800

    security(terraform): enforce DMARC quarantine, restrict HTTP/S to Cloudflare IPs, fix tfvars example (#64)
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -146,16 +146,34 @@ resource "digitalocean_firewall" "production" {
 
   # HTTP
   inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    protocol   = "tcp"
+    port_range = "80"
+    source_addresses = [
+      # Cloudflare IPv4 ranges — https://www.cloudflare.com/ips-v4
+      "173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22",
+      "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20",
+      "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13",
+      "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22",
+      # Cloudflare IPv6 ranges — https://www.cloudflare.com/ips-v6
+      "2400:cb00::/32", "2606:4700::/32", "2803:f800::/32", "2405:b500::/32",
+      "2405:8100::/32", "2a06:98c0::/29", "2c0f:f248::/32"
+    ]
   }
 
   # HTTPS
   inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    protocol   = "tcp"
+    port_range = "443"
+    source_addresses = [
+      # Cloudflare IPv4 ranges — https://www.cloudflare.com/ips-v4
+      "173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22",
+      "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20",
+      "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13",
+      "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22",
+      # Cloudflare IPv6 ranges — https://www.cloudflare.com/ips-v6
+      "2400:cb00::/32", "2606:4700::/32", "2803:f800::/32", "2405:b500::/32",
+      "2405:8100::/32", "2a06:98c0::/29", "2c0f:f248::/32"
+    ]
   }
 
   # SSH (restricted)
