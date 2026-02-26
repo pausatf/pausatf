2aca09f6578de16ac9615a90726a02785a1c90ac
Thomas Vincent <thomasvincent@gmail.com>
2026-02-25 20:42:16 -0800

    test(terraform): add database module test fixture

---
+++ terraform/modules/digitalocean/database/tests/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.76"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Token sourced from DIGITALOCEAN_ACCESS_TOKEN env var during tests
+}
+
+module "database" {
+  source = "../"
+
+  name            = var.test_name
+  engine          = var.test_engine
+  engine_version  = var.test_engine_version
+  size            = var.test_size
+  region          = var.test_region
+  node_count      = var.test_node_count
+  environment     = var.test_environment
+}
+
+output "cluster_name" {
+  value = module.database.name
+}
+
+output "cluster_urn" {
+  value = module.database.urn
+}
