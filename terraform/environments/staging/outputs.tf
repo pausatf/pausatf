c3b9f2540e3d6849f726eeb16a6ea82cb7411b20
Thomas Vincent <thomasvincent@gmail.com>
2026-02-27 22:49:35 -0800

    feat(terraform,ansible): add managed DB outputs and variables
    Add database_user, database_password, and database_name outputs to the
    wordpress stack and both environment configurations. Add managed_db_*
    variables to Ansible group_vars for the migration playbook to consume.

--- terraform/environments/staging/outputs.tf
+++ terraform/environments/staging/outputs.tf
@@ -35,6 +35,28 @@ output "database_uri" {
   sensitive   = true
 }
 
+output "database_port" {
+  description = "Staging database port"
+  value       = module.wordpress.database_port
+}
+
+output "database_user" {
+  description = "Staging database admin username"
+  value       = module.wordpress.database_user
+  sensitive   = true
+}
+
+output "database_password" {
+  description = "Staging database admin password"
+  value       = module.wordpress.database_password
+  sensitive   = true
+}
+
+output "database_name" {
+  description = "Staging database default database name"
+  value       = module.wordpress.database_name
+}
+
 output "firewall_id" {
   description = "Staging firewall ID"
   value       = module.wordpress.firewall_id
