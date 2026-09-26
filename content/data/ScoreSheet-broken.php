129d410a5f746309a9b55295fec0e0d8c95917ea
Thomas Vincent <thomasvincent@gmail.com>
2026-09-26 02:53:17 -0700

    fix(php): use PHPCS indentation for score sheet repair

--- content/data/ScoreSheet-broken.php
+++ content/data/ScoreSheet-broken.php
@@ -711,11 +711,11 @@ CREATE TABLE IF NOT EXISTS score_sheet (
   function get_to_emails($series) {
     // modify the following to change who gets what submitted team sheets
     $to_emails = array(
-        // 'all' => 'tlbernhard@att.net;tct3@pge.com;jeff@teeters.us',
-        'all'  => '',
-        'xc'   => 'lesong@tkecapital.com',
-        'road' => 'tlbernhard2@gmail.com',
-    );
+		// 'all' => 'tlbernhard@att.net;tct3@pge.com;jeff@teeters.us',
+		'all'  => '',
+		'xc'   => 'lesong@tkecapital.com',
+		'road' => 'tlbernhard2@gmail.com',
+	);
     $series_xlate = array('long' => 'road', 'short' => 'road', 'xc' => 'xc');
     $series_to = $to_emails[$series_xlate[strtolower($series)]];
     $to_list = array();
